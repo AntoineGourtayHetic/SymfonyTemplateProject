fe13a454c33282245b6a2ebcd145cb55bf931a71
Antoine Gourtay <antoine.gourtay@hetic.net>
2017-01-27 17:18:33 +0100

    Adding Controller

---
+++ src/AppBundle/Controller/StudentController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class StudentController extends Controller
+{
+    /**
+     * @Route("/index")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $data = $em->getRepository('AppBundle:Student')->displayAges();
+        dump($data);
+        return $this->render('Student/index.html.twig', array(
+
+        ));
+    }
+
+}
